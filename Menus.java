4b74b63d2ec3d4fadd2880e97a1a6867f385b0bd
RafaLinares <Rafa.Linares.Molina@gmail.com>
2014-09-11 13:00:56 +0200

    Añadido Lorem Ipsum

--- Menus.java
+++ Menus.java
@@ -3,6 +3,8 @@ public class Menus{
   public static void main(){
   System.out.println("Que menus quieres?");
   //Faltan cosas : )
+  System.out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit," 
+  +"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
   }
 
 }
