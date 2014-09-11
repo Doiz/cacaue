6003894237652338267962886bbadfaad9a9f455
Doiz <ludoro@campusaula.com>
2014-09-11 11:00:49 +0200

    Añadidas algunas chorradas

--- Menus.java
+++ Menus.java
@@ -1,8 +1,10 @@
 public class Menus{
 
-  public static void main(){
+  public static void main(String[] args){
   System.out.println("Que menus quieres?");
+  System.out.println("Los quiero todos!!!");
   //Faltan cosas : )
+  //Pues ponlas FDO:David Izquierdo :D
   }
 
 }
