879238f2d54ef8068c28439923030df39b471529
Doiz <ludoro@campusaula.com>
2014-09-11 11:17:43 +0200

    nueva1

--- Menus.java
+++ Menus.java
@@ -3,6 +3,7 @@ public class Menus{
   public static void main(String[] args){
   System.out.println("Que menus quieres?");
   System.out.println("Los quiero todos!!!");
+  System.out.println("Los quiero todos!!!");
   //Faltan cosas : )
   //Pues ponlas FDO:David Izquierdo :D
   }
